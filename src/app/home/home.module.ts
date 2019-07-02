cb79f7ae51f3888725bf4cc2a82455898de1df58
Valery Semenenko <gearmobile@gmail.com>
2019-07-02 10:04:45 +0300

    fix: change home component background

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// MODULES
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../@shared/shared.module';
+
+// COMPONENTS
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
